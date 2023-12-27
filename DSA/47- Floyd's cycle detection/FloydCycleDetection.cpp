9872aa49d70c45d5f435e1adf74c0f2d68a7ddf5
Akshat Jain <akshatcodek@gmail.com>
2023-12-28 02:02:46 +0530

    26-27DEC

--- DSA/47- Floyd's cycle detection/FloydCycleDetection.cpp
+++ DSA/47- Floyd's cycle detection/FloydCycleDetection.cpp
@@ -1,5 +1,7 @@
 // S.C -> O(1),  T.C -> O(n)
-
+// 1) We Find loop 
+// 2) Get start
+// 3) remove loop
 #include<bits/stdc++.h>
 using namespace std;
 
@@ -25,29 +27,63 @@ class Node{
 };
 
 
-bool floydDetection(Node* head){
+Node* floydCycleDetection(Node* head){
     if( head == NULL){
-        return false;
+        return NULL;
     }
     Node* tortoise = head;
-    Node* Hare = head;
+    Node* hare = head;
 
     while (tortoise != NULL && hare != NULL  )
     {
 
         tortoise = tortoise -> next;
-        hare = hare -> next
+        hare = hare -> next;
         if( hare != NULL ){
             hare = hare -> next;
         }
 
         if( tortoise == hare ){
-            return 1;
+            return hare;
         }
 
     }
     
-    return 0;
+    return NULL;
+}
+
+Node* getStart(Node* head){
+    if(head == NULL){
+        return NULL;
+    }
+    Node* intersection = floydCycleDetection(head);
+
+// Checking if loop is present
+    if(intersection == NULL){
+        return NULL;
+    }
+    Node* slow = head;
+    while (slow != intersection )
+    {
+        slow = slow -> next;
+        intersection = intersection -> next;
+    }
+
+    return slow;   
+}
+
+void removeLoop(Node* head){
+    Node* start = getStart(head);
+    Node* temp = start;
+    // Checking if loop is present
+    if(start == NULL){
+        return;
+    }
+    while (temp -> next != start)
+    {
+        temp = temp-> next;
+    }
+    temp -> next = NULL;
 }
 
 int main(){
