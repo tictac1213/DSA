f22bce1ff07dcd7f36e72a56ae5584a8db8eaf22
Akshat Jain <akshatcodek@gmail.com>
2023-12-28 02:13:56 +0530

    Rearrangement

---
+++ Rndm/test.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+
+
+int main(){
+    std::cout << std::setw(10) << std::left << "Name" << std::setw(15) << "Age" << std::endl;
+std::cout << std::setw(10) << "Alice" << std::setw(15) << 30 << std::endl;
+
+
+
+return 0;
+}
